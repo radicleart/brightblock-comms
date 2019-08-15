a048fda63a29a6dcefe550750780ece9ef8459b9
Mike Cohen <mjoecohen@gmail.com>
2019-08-15 13:35:25 +0100

    staging: setup staging / production environments

--- src/main/java/org/brightblock/comms/conf/ApplicationSettings.java
+++ src/main/java/org/brightblock/comms/conf/ApplicationSettings.java
@@ -1,8 +1,12 @@
 package org.brightblock.comms.conf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.brightblock.comms.conf.settings.DomainModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +22,33 @@ public class ApplicationSettings {
 	private String blockstackOrgBase;
 	private List<DomainModel> domains;
 	private String mongoIp;
+	private String containerHostIp;
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
+	public ApplicationSettings() {
+		super();
+        try {
+    		Process p = Runtime.getRuntime().exec("ip route show");
+            
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            // read the output from the command
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println("Found: " + s);
+            	if (s.indexOf("default") > -1) {
+            		String[] parts = s.split(" ");
+            		containerHostIp = parts[2];
+            	}
+            }
+            System.out.println("Found host IP=" + containerHostIp);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+        }
+	}
 
 	public String getBlockstackBase() {
 		return blockstackBase;
@@ -68,7 +99,12 @@ public class ApplicationSettings {
 	}
 
 	public String getMongoIp() {
-		return mongoIp;
+        System.out.println("ACTIVE SPRING PROFILE = " + activeProfile);
+		if (activeProfile.equals("staging") || activeProfile.equals("production")) {
+        	return containerHostIp;
+		} else {
+    		return mongoIp;
+        }
 	}
 
 	public void setMongoIp(String mongoIp) {
