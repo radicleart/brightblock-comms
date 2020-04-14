87cc7dc8734627f01e9d8be2272be4bd82166590
Mike Cohen <mjoecohen@gmail.com>
2020-04-14 07:04:59 +0100

    idea intellij trial

--- src/main/java/org/brightblock/sidecar/common/GlobalExceptionHandler.java
+++ src/main/java/org/brightblock/sidecar/common/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
